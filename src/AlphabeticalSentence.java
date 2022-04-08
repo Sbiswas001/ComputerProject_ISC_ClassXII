ada05977619186d1d3a4dea188f6c575544fe3fd
Sayan Biswas <sbiswas001.tech@gmail.com>
2022-04-08 18:21:56 +0530

    new

---
+++ src/AlphabeticalSentence.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+/**
+ * Classname: AlphabeticalSentence
+ * This class enters multiple strings from user and arranges them in ascending order.
+ * @author Sayan Biswas
+ * @version 08.04.2022
+ */
+public class AlphabeticalSentence {
+    /**
+     * number Stores strings
+     */
+    private String[] s;
+
+    /**
+     * Initializes instance variables
+     */
+    private AlphabeticalSentence() {
+        s = null;
+    }
+
+    /**
+     * Inputs strings from user and stores them into an array
+     */
+    private void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of strings: ");
+        if(!sc.hasNextInt()){//Deals with InputMismatchException
+            System.out.print("Wrong datatype; enter a number: ");
+            input();
+        }
+        int x=Integer.parseInt(sc.next());
+        s = new String[x];
+        System.out.print("Enter the strings: ");
+        for (int i = 0; i < x; i++) {
+            s[i] = sc.next();
+        }
+    }
+
+    /**
+     * Arranges the strings in alphabetical order
+     * @param a Strings to be sorted
+     */
+    private void compute(String[] a) {
+        String temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i].toUpperCase().compareTo(a[j].toUpperCase())>0) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * Displays the sorted strings
+     */
+    private void display() {
+
+        for(int i=0; i<s.length; i++) {
+            System.out.print(s[i]+" ");
+        }
+    }
+
+    /**
+     * Calls other methods
+     */
+    public static void main(String[] args) {
+        AlphabeticalSentence ob = new AlphabeticalSentence();
+        ob.input();
+        ob.compute(ob.s);
+        ob.display();
+    }
+}
+
