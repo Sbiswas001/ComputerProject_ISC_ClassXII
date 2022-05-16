19f588332968f73118a83b511ab45f66550343cf
Sayan Biswas <sbiswas001.tech@gmail.com>
2022-05-16 09:56:50 +0530

    Update AscendingOrder.java

--- src/com/sbiswas001/twelveproject/AscendingOrder.java
+++ src/com/sbiswas001/twelveproject/AscendingOrder.java
@@ -43,13 +43,13 @@ public class AscendingOrder {
      */
     private int[] sort(int[] a) {
         int temp;
-        int n=a.length;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n-i-1;j++) {
-                if(a[j]>a[j+1]) {
-                    temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
+        int n = a.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if(a[j] > a[j+1]) {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
                 }
             }
         }
